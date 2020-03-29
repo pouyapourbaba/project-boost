39f2d5f612a9dac1ba534863ba383b0b0c642345
Pouya Pourbaba <p.pourbaba@gmail.com>
2020-03-29 19:16:33 +0300

    Rotate and thrust the ship

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rigidBody;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward * Time.deltaTime);
+        }
+    }
+}
