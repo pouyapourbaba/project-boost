d0f726dd7aac3eb71c198a17d29b754742a19728
Pouya Pourbaba <p.pourbaba@gmail.com>
2020-03-31 20:55:32 +0300

    Add an audio source to the Rocket and make it play when thrusting the rocket

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update() {
@@ -16,6 +18,11 @@ public class Rocket : MonoBehaviour {
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else  {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A)) {
