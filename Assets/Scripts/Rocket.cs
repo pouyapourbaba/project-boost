4edc2b9ac1861bd2e384db5c9c248bd3c3c52e55
Pouya Pourbaba <p.pourbaba@gmail.com>
2020-04-03 21:23:08 +0300

    Use serialized varaibales in the script and modify their values from the editor

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
     Rigidbody rigidBody;
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f; // reaction control system
+    [SerializeField] float mainThrust = 50f;
 
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
@@ -16,19 +18,31 @@ public class Rocket : MonoBehaviour {
     }
 
     private void ProcessInput() {
+        thrust();
+        rotate();
+    }
+
+    private void thrust() {
         if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying) {
                 audioSource.Play();
             }
-        } else  {
+        } else {
             audioSource.Stop();
         }
+    }
+
+    private void rotate() {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward * Time.deltaTime);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         } else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
